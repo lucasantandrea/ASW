89fb4291eaaae41e37542378c35c07102df8f397
lucasantandrea <santandrea.luca@gmail.com>
2014-12-31 15:29:11 +0100

    aggiunta logout servlet

--- WebApplication/src/java/asw1025/LoginServlet.java
+++ WebApplication/src/java/asw1025/LoginServlet.java
@@ -1,8 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*    
+    Esame ASW 2014-2015
+    Autori: Luca Santandrea
+    Matricola: 0900050785
+*/
 package asw1025;
 
 import java.io.BufferedInputStream;
